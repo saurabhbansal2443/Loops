c86b2b9b366452f468568af8a96c50a7b18257f7
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-04 15:05:44 +0530

    loops started

---
+++ printTable.java
@@ -0,0 +1,11 @@
+public class printTable {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i=1;i<=10;i++){
+
+            System.out.println(n + " X " + i + " = " + n*i);
+        }
+    }
+}
+ 
